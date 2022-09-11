49c1bff54775226dc84bbaa024faceb63ec3d5ec
TÖRÖK Attila <torokati44@gmail.com>
2022-09-12 01:31:31 +0200

    audio: recreate-stream??

--- native/src/audio.rs
+++ native/src/audio.rs
@@ -1,6 +1,7 @@
+use ndk::audio::{AudioError, AudioErrorResult, AudioStream};
 use ruffle_core::backend::audio::{
-    swf, AudioBackend, AudioMixer, DecodeError, RegisterError, SoundHandle, SoundInstanceHandle,
-    SoundTransform,
+    swf, AudioBackend, AudioMixer, AudioMixerProxy, DecodeError, RegisterError, SoundHandle,
+    SoundInstanceHandle, SoundTransform,
 };
 use ruffle_core::impl_audio_mixer_backend;
 
@@ -12,16 +13,14 @@ pub struct AAudioAudioBackend {
 type Error = Box<dyn std::error::Error>;
 
 impl AAudioAudioBackend {
-    pub fn new() -> Result<Self, Error> {
-        let mixer = AudioMixer::new(2, 44100);
+    fn make_stream(mixer: &AudioMixer) -> Result<AudioStream, Error> {
         let pr = mixer.proxy();
-        let stream = ndk::audio::AudioStreamBuilder::new()?
+        ndk::audio::AudioStreamBuilder::new()?
             .direction(ndk::audio::AudioDirection::Output)
             .format(ndk::audio::AudioFormat::PCM_I16)
             .channel_count(2)
             .sample_rate(44100)
-            .frames_per_data_callback(4096)
-            .performance_mode(ndk::audio::AudioPerformanceMode::PowerSaving)
+            .performance_mode(ndk::audio::AudioPerformanceMode::LowLatency)
             .data_callback(Box::new(move |_stream, data, len| {
                 let mut sl = unsafe {
                     std::slice::from_raw_parts_mut::<i16>(data as *mut i16, len as usize * 2)
@@ -29,8 +28,19 @@ impl AAudioAudioBackend {
                 pr.mix(&mut sl);
                 ndk::audio::AudioCallbackResult::Continue
             }))
-            .open_stream()?;
+            .error_callback(Box::new(move |&mut stream, error| {
+                if error == AudioError::ErrorResult(AudioErrorResult::Disconnected) {
+                    stream.request_stop();
+                    stream = Self::make_stream(mixer);
+                }
+            }))
+            .open_stream()
+    }
 
+    pub fn new() -> Result<Self, Error> {
+        let mixer = AudioMixer::new(2, 44100);
+
+        let stream = Self::make_stream(&mixer)?;
         Ok(Self { stream, mixer })
     }
 }
