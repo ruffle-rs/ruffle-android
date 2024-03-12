7f4858441dc9ed54740ee7e476efb47c65e833c8
Nathan Adams <dinnerbone@dinnerbone.com>
2024-03-12 20:47:00 +0100

    native: Cargo fmt

--- native/src/custom_event.rs
+++ native/src/custom_event.rs
@@ -7,7 +7,11 @@ use ruffle_core::ViewportDimensions;
 pub enum RuffleEvent {
     /// Indicates that one or more tasks are ready to poll on our executor.
     TaskPoll,
-    VirtualKeyEvent {down: bool, key_code: KeyCode, key_char: Option<char>},
+    VirtualKeyEvent {
+        down: bool,
+        key_code: KeyCode,
+        key_char: Option<char>,
+    },
     RunContextMenuCallback(usize),
     ClearContextMenu,
     Resize(ViewportDimensions),
