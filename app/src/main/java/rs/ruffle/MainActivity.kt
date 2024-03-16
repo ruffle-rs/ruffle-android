e395c087b4f3760042da741bd0db139b1a226c99
Nathan Adams <dinnerbone@dinnerbone.com>
2024-03-16 22:48:03 +0100

    Restructure everything, native is now top level and app is written in kotlin with new Jetpack Compose ui

---
+++ app/src/main/java/rs/ruffle/MainActivity.kt
@@ -0,0 +1,44 @@
+package rs.ruffle
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import rs.ruffle.ui.theme.RuffleTheme
+import java.io.IOException
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            RuffleTheme {
+                RuffleNavHost(openSwf = { openSwf(it) })
+            }
+        }
+    }
+
+    private fun openSwf(uri: Uri) {
+        val resolver = contentResolver
+        try {
+            val inputStream = resolver.openInputStream(uri)
+            inputStream.use {
+                val available = inputStream!!.available()
+                val bytes = ByteArray(available)
+                // assuming the whole contents will be available at once
+                inputStream.read(bytes)
+                FullscreenNativeActivity.SWF_BYTES = bytes
+            }
+        } catch (_: IOException) {
+        }
+
+        val intent = Intent(
+            this@MainActivity,
+            FullscreenNativeActivity::class.java
+        )
+        startActivity(intent)
+    }
+}
