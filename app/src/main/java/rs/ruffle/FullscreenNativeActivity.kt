352a977fc686f6e92034e5748a1c1c5b72480789
Nathan Adams <dinnerbone@dinnerbone.com>
2024-03-17 23:54:31 +0100

    Convert FullscreenNativeActivity to kotlin

---
+++ app/src/main/java/rs/ruffle/FullscreenNativeActivity.kt
@@ -0,0 +1,237 @@
+package rs.ruffle
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.PopupMenu
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.google.androidgamesdk.GameActivity
+import java.io.DataInputStream
+import java.io.IOException
+
+class FullscreenNativeActivity : GameActivity() {
+    @Suppress("unused")
+    // Used by Rust
+    private val swfBytes: ByteArray?
+        get() {
+            if (intent.data == null) {
+                return null
+            }
+            try {
+                contentResolver.openInputStream(intent.data!!).use { inputStream ->
+                    if (inputStream == null) {
+                        return null
+                    }
+                    val bytes = ByteArray(inputStream.available())
+                    val dataInputStream = DataInputStream(inputStream)
+                    dataInputStream.readFully(bytes)
+                    return bytes
+                }
+            } catch (ignored: IOException) {
+            }
+            return null
+        }
+
+    @Suppress("unused")
+    // Used by Rust
+    private fun navigateToUrl(url: String?) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+
+    private var loc = IntArray(2)
+
+    @Suppress("unused")
+    // Handle of an EventLoopProxy over in rust-land
+    private val eventLoopHandle: Long = 0
+
+    @Suppress("unused")
+    // Used by Rust
+    private val locOnScreen: IntArray
+        get() {
+            mSurfaceView.getLocationOnScreen(loc)
+            return loc
+        }
+
+    @Suppress("unused")
+    // Used by Rust
+    private val surfaceWidth: Int
+        get() = mSurfaceView.width
+
+    @Suppress("unused")
+    // Used by Rust
+    private val surfaceHeight: Int
+        get() = mSurfaceView.height
+
+    private external fun keydown(keyCode: Byte, keyChar: Char)
+    private external fun keyup(keyCode: Byte, keyChar: Char)
+    private external fun requestContextMenu()
+    private external fun runContextMenuCallback(index: Int)
+    private external fun clearContextMenu()
+
+    @Suppress("unused")
+    // Used by Rust
+    private fun showContextMenu(items: Array<String>) {
+        runOnUiThread {
+            val popup = PopupMenu(this, findViewById(R.id.button_cm))
+            val menu = popup.menu
+            menu.setGroupDividerEnabled(true)
+            var group = 1
+            for (i in items.indices) {
+                val elements = items[i].split(" ".toRegex(), limit = 4).toTypedArray()
+                val enabled = elements[0].toBoolean()
+                val separatorBefore = elements[1].toBoolean()
+                val checked = elements[2].toBoolean()
+                val caption = elements[3]
+                if (separatorBefore) group += 1
+                val item = menu.add(group, i, Menu.NONE, caption)
+                item.setEnabled(enabled)
+                if (checked) {
+                    item.setCheckable(true)
+                    item.setChecked(true)
+                }
+            }
+            popup.setOnMenuItemClickListener { item: MenuItem ->
+                runContextMenuCallback(item.itemId)
+                true
+            }
+            popup.setOnDismissListener { pm: PopupMenu? -> clearContextMenu() }
+            popup.show()
+        }
+    }
+
+    override fun onCreateSurfaceView() {
+        val inflater = layoutInflater
+        val layout = inflater.inflate(R.layout.keyboard, null) as ConstraintLayout
+        contentViewId = ViewCompat.generateViewId()
+        layout.id = contentViewId
+        setContentView(layout)
+        mSurfaceView = InputEnabledSurfaceView(this)
+        val placeholder = findViewById<View>(R.id.placeholder)
+        val pars = placeholder.layoutParams as ConstraintLayout.LayoutParams
+        val parent = placeholder.parent as ViewGroup
+        val index = parent.indexOfChild(placeholder)
+        parent.removeView(placeholder)
+        parent.addView(mSurfaceView, index)
+        mSurfaceView.setLayoutParams(pars)
+        val keys = gatherAllDescendantsOfType<Button>(
+            layout.getViewById(R.id.keyboard),
+            Button::class.java
+        )
+        for (b in keys) {
+            b.setOnTouchListener { view: View, motionEvent: MotionEvent ->
+                val tag = view.tag as String
+                if (tag != null) {
+                    val spl = tag.split(" ".toRegex(), limit = 2).toTypedArray()
+                    val by = spl[0].toByte()
+                    val c: Char = if (spl.size > 1) spl[1][0] else Char.MIN_VALUE
+                    if (motionEvent.action == MotionEvent.ACTION_DOWN) keydown(by, c)
+                    if (motionEvent.action == MotionEvent.ACTION_UP) keyup(by, c)
+                }
+                false
+            }
+        }
+        layout.findViewById<View>(R.id.button_kb).setOnClickListener { view: View? ->
+            val keyboard = layout.getViewById(R.id.keyboard)
+            if (keyboard.visibility == View.VISIBLE) {
+                keyboard.visibility = View.GONE
+            } else {
+                keyboard.visibility = View.VISIBLE
+            }
+        }
+        layout.findViewById<View>(R.id.button_cm)
+            .setOnClickListener { view: View? -> requestContextMenu() }
+        layout.requestLayout()
+        layout.requestFocus()
+        mSurfaceView.holder.addCallback(this)
+        ViewCompat.setOnApplyWindowInsetsListener(mSurfaceView, this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val keyboard = findViewById<View>(R.id.keyboard)
+        val isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE
+        keyboard.visibility = if (isLandscape) View.GONE else View.VISIBLE
+    }
+
+    private fun hideSystemUI() {
+        // This will put the game behind any cutouts and waterfalls on devices which have
+        // them, so the corresponding insets will be non-zero.
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+        }
+        // From API 30 onwards, this is the recommended way to hide the system UI, rather than
+        // using View.setSystemUiVisibility.
+        val decorView = window.decorView
+        val controller = WindowInsetsControllerCompat(
+            window,
+            decorView
+        )
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.hide(WindowInsetsCompat.Type.displayCutout())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // When true, the app will fit inside any system UI windows.
+        // When false, we render behind any system UI windows.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        hideSystemUI()
+        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
+        // We set the fields in native_engine.cpp.
+        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
+        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
+        requestNoStatusBarFeature()
+        super.onCreate(savedInstanceState)
+    }
+
+    val isGooglePlayGames: Boolean
+        get() {
+            val pm = packageManager
+            return pm.hasSystemFeature("com.google.android.play.feature.HPE_EXPERIENCE")
+        }
+
+    private fun requestNoStatusBarFeature() {
+        // Hiding the status bar this way makes it see through when pulled down
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+    companion object {
+        init {
+            // load the native activity
+            System.loadLibrary("ruffle_android")
+        }
+
+        private fun <T> gatherAllDescendantsOfType(v: View, t: Class<*>): List<T> {
+            val result: MutableList<T> = ArrayList()
+            if (t.isInstance(v)) result.add(v as T)
+            if (v is ViewGroup) {
+                val vg = v
+                for (i in 0 until vg.childCount) {
+                    result.addAll(gatherAllDescendantsOfType(vg.getChildAt(i), t))
+                }
+            }
+            return result
+        }
+    }
+}
