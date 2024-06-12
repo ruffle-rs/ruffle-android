050db3ec5132881cd83a56350a937780a11eaa1b
soundsnow <109885044+soundsnow@users.noreply.github.com>
2024-06-12 23:16:06 +0800

    Add files via upload

--- MagicDosBox/MagicBox/src/main/assets/Shaders/normal/normal_fs.glsl
+++ MagicDosBox/MagicBox/src/main/assets/Shaders/normal/normal_fs.glsl
@@ -1,1 +1,12 @@
+precision mediump float;
+varying vec2 v_texCoord;
+uniform sampler2D s_texture;
+uniform float alpha;
 
+void main() 
+{
+    //gl_FragColor = vec4(texture2D(s_texture, v_texCoord).xyz, texture2D(s_texture, v_texCoord).w * alpha);
+	gl_FragColor = texture2D( s_texture, v_texCoord );
+	//gl_FragColor = vec4(gl_FragColor.xyz, gl_FragColor.w * alpha);
+	gl_FragColor.a = gl_FragColor.w * alpha;
+}
