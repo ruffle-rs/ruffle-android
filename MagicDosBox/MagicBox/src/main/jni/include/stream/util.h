ec85128170c0244cbc6cc8b88909673ee2dd28f2
soundsnow <109885044+soundsnow@users.noreply.github.com>
2024-06-12 22:58:28 +0800

    Add files via upload

--- MagicDosBox/MagicBox/src/main/jni/include/stream/util.h
+++ MagicDosBox/MagicBox/src/main/jni/include/stream/util.h
@@ -1,1 +1,16 @@
+/* Programmer: Nicholas Wertzberger
+ * 		Email: wertnick@gmail.com
+ *
+ * A listing of helper functions for integrating with the JNI.
+ */
+#ifndef _STREAM_UTIL_H
+#define _STREAM_UTIL_H
 
+enum {
+	BAD_MEM = -1,
+};
+
+void
+JNU_ThrowByName(JNIEnv *env, const char *name, const char *msg, int code);
+
+#endif
