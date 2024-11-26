8ff102a12c698f2cf4ad4911315a1ebd51af6a9c
TÖRÖK Attila <torokati44@gmail.com>
2024-11-26 18:43:19 +0100

    Update compile and target SDK version (API level) to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ plugins {
 
 android {
     namespace = "rs.ruffle"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "rs.ruffle"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
