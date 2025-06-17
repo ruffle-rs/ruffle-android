1eedde25f211d517a94002e1e921b841ffaad13f
TÖRÖK Attila <torokati44@gmail.com>
2025-06-17 15:53:35 +0200

    Check in proper (initial) versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "rs.ruffle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 250616
+        versionName = "0.250616"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
