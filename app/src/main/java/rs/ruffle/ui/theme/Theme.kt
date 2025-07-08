b3723aa9dfa7221a76d77f4a942bcdc1eb910492
TÖRÖK Attila <torokati44@gmail.com>
2025-07-08 11:27:21 +0200

    Fix new ktlint lints

--- app/src/main/java/rs/ruffle/ui/theme/Theme.kt
+++ app/src/main/java/rs/ruffle/ui/theme/Theme.kt
@@ -263,9 +263,7 @@ fun RuffleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
-    content:
-    @Composable()
-    () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
