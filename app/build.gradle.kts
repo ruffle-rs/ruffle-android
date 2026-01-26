6ae5a0322f3d22ab415990de657df991392868a7
RuffleBuild <ruffle@ruffle.rs>
2026-01-26 15:18:10 +0000

    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "rs.ruffle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 260119
-        versionName = "0.260119"
+        versionCode = 260126
+        versionName = "0.260126"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
